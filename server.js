36957d32281d6c3897f2856d6f84e7cb32136d0c
Mohammed Roshan <mroshan4ever@gmail.com>
2026-03-23 14:11:05 +0900

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = 3000;
 const API_KEY = process.env.GEMINI_API_KEY;
 const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PATCH, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') { res.writeHead(204); res.end(); return; }
